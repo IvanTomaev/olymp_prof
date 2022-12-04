1c2d1fa5ddd2c8ae0eb7c62ae0507bd799de0c79
IvanTomaev <ivam.tomaev.01@gmail.com>
2022-12-04 18:29:30 +0300

    update on 10

--- 10.py
+++ 10.py
@@ -1,26 +1,22 @@
 length = int(input())
 array = list(map(int, input().split()))
+ones = array.count(1)
 result = []
-maxLength=1
-for i in array:
-    if i == 1:
-        n= array.count(1)
-        if n > maxLength:
-            maxLength = n
-            result = 1
-        continue
-    current = i
-    n=1
-    while current**2 in array:
-        current = current**2
-        n+=1
-    if n>maxLength:
-        maxLength = n
-        result = i
-array = [result]
-for i in range(maxLength-1):
-   array.append(array[-1]**2)
-print(maxLength)
-print(" ".join(map(str, array)))
+result.append([1 for i in range(ones)])
 
+numbers = (i for i in array if (int(i**0.5)**2 == i or (i**2) in array) and i != 1)
+
+for i in numbers:
+    current = i
+    buffer = [current]
+    while current ** 2 in array:
+        while current in array:
+            array.remove(current)
+        current = current ** 2
+        buffer.append(current)
+    result.append(buffer)
+
+result = sorted(result, key=lambda x:len(x), reverse=True)
+print(len(result[0]))
+for i in result[0]: print(i, end = " ")
 
