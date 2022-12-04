538dbb2017acbb1af3c1ce0d6145bffcd27f27d5
IvanTomaev <ivam.tomaev.01@gmail.com>
2022-12-04 18:04:53 +0300

    second commit

--- 10.py
+++ 10.py
@@ -1,12 +1,26 @@
-def checkThird(num):
-    powers = [0]
-    num -=1
-    if num % 2 ==0:
-        return False
-    
+length = int(input())
+array = list(map(int, input().split()))
+result = []
+maxLength=1
+for i in array:
+    if i == 1:
+        n= array.count(1)
+        if n > maxLength:
+            maxLength = n
+            result = 1
+        continue
+    current = i
+    n=1
+    while current**2 in array:
+        current = current**2
+        n+=1
+    if n>maxLength:
+        maxLength = n
+        result = i
+array = [result]
+for i in range(maxLength-1):
+   array.append(array[-1]**2)
+print(maxLength)
+print(" ".join(map(str, array)))
 
 
-
-number = int(input())
-if checkThird(number):
-    print('YES')
