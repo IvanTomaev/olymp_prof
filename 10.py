b4ff3f9b457de0d900b4f0b4c08ff4d745026c13
IvanTomaev <ivam.tomaev.01@gmail.com>
2022-12-04 18:40:06 +0300

    update on 10

--- 10.py
+++ 10.py
@@ -1,22 +1,34 @@
+def generatSequence(array):
+    ones = array.count(1)
+    result = []
+    result.append([1 for i in range(ones)])
+    numbers = [i for i in array if (int(i ** 0.5) ** 2 == i or (i ** 2) in array) and i != 1]
+    if not numbers and not result:
+        print(1)
+        print(array[0])
+        return 0
+    else:
+        for i in numbers:
+            current = i
+            buffer = [current]
+            while current ** 2 in array:
+                while current in array:
+                    array.remove(current)
+                current = current ** 2
+                buffer.append(current)
+            result.append(buffer)
+
+        result = sorted(result, key=lambda x: len(x), reverse=True)
+        print(len(result[0]))
+        for i in result[0]: print(i, end=" ")
+        return 1
+
+
+
 length = int(input())
 array = list(map(int, input().split()))
-ones = array.count(1)
-result = []
-result.append([1 for i in range(ones)])
 
-numbers = (i for i in array if (int(i**0.5)**2 == i or (i**2) in array) and i != 1)
+i = generatSequence(array)
 
-for i in numbers:
-    current = i
-    buffer = [current]
-    while current ** 2 in array:
-        while current in array:
-            array.remove(current)
-        current = current ** 2
-        buffer.append(current)
-    result.append(buffer)
 
-result = sorted(result, key=lambda x:len(x), reverse=True)
-print(len(result[0]))
-for i in result[0]: print(i, end = " ")
 
